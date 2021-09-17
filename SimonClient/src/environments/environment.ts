59d594990c4e40f215188df5ca4a61aea21fe4f2
syoung908 <syoung908@gmail.com>
2021-09-16 20:41:18 -0700

    Update environment.ts

--- SimonClient/src/environments/environment.ts
+++ SimonClient/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   api: {
-    url: "https://localhost:44302"
+    //url: "https://localhost:44302"
+    url: "https://simonsignalr.azurewebsites.net"
   }
 };
 
